5d9f9c5722e689fb76b9efd2040f789a06173fb7
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-09-10 16:40:15 +0300

    Update start.py

--- ZelzalMusic/plugins/bot/start.py
+++ ZelzalMusic/plugins/bot/start.py
@@ -19,7 +19,7 @@ from config import (OWNER_ID ,
 	         BOT_TOKEN,
 	         BANNED_USERS)
 from strings.filters import command
-from strings import command, get_string
+from strings import get_string
 from ZelzalMusic import Telegram, YouTube, app
 from ZelzalMusic.misc import SUDOERS, _boot_
 from ZelzalMusic.plugins.playlist import del_plist_msg
